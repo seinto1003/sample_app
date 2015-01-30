104f227fee580ab6da160d8fb68f2930a80c94e2
seinto <seinto@mail.seinto1003.com>
2015-01-31 00:20:17 +0900

    Initial commit

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.2/ext/json/siteconf20150130-1370-jn0ya.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150130-1370-xgn4n8"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
