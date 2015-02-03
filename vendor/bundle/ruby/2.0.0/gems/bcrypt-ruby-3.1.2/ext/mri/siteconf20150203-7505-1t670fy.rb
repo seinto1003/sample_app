e52491f03a7f6aed6a0656cba648ab4f16e65915
seinto <seinto@mail.seinto1003.com>
2015-02-03 18:20:15 +0900

    Make a basic User model (including secure passwords)

---
+++ vendor/bundle/ruby/2.0.0/gems/bcrypt-ruby-3.1.2/ext/mri/siteconf20150203-7505-1t670fy.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150203-7505-1ad9dpd"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
